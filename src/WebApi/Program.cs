122eab5bdb47b12f84c8d0629b3722b9dabd1c97
AmpF5 <lolc44t@gmail.com>
2024-09-05 20:19:05 +0200

    add references, serilog and fluentValidation

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,4 +1,6 @@
 
+using Serilog;
+
 namespace WebApi;
 
 public class Program
@@ -7,27 +9,28 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
+        builder.Host.UseSerilog((context, configuration) => {
+            configuration.ReadFrom.Configuration(context.Configuration);
+        });
+
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
 
+        app.UseSerilogRequestLogging();
+            
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
-
-
+        
         app.MapControllers();
 
         app.Run();
